a9c42030a647fc50b88e26b3ade65277cb844381
boss <boss@boss-Inspiron-1545.(none)>
2013-03-03 17:16:06 -0800

    fix conflicts

--- drivers/gpu/msm/adreno_ringbuffer.h
+++ drivers/gpu/msm/adreno_ringbuffer.h
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
 /* Copyright (c) 2002,2007-2012, Code Aurora Forum. All rights reserved.
-=======
-/* Copyright (c) 2002,2007-2013, The Linux Foundation. All rights reserved.
->>>>>>> 740f674... msm: kgsl: Remove duplicate code for checking timestamps
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 and
