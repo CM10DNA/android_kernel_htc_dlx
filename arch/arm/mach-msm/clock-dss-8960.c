70c5ab23a9ccfaccd43e2104539929e008cc296a
boss <boss@boss-Inspiron-1545.(none)>
2013-03-03 17:55:12 -0800

    fix conflicts

--- arch/arm/mach-msm/clock-dss-8960.c
+++ arch/arm/mach-msm/clock-dss-8960.c
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
 /* Copyright (c) 2009-2011, Code Aurora Forum. All rights reserved.
-=======
-/* Copyright (c) 2009-2013, The Linux Foundation. All rights reserved.
->>>>>>> e67749b... msm_fb: hdmi: Support for 1280x1024@60Hz resolution
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 and
